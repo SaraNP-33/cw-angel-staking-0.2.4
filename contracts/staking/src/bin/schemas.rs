b6ead62ee1582154657eebd601199f7a82833a0f
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2023-01-09 14:22:18 -0100

    fixed issue with schema.rs in staking contract

--- contracts/staking/src/bin/schemas.rs
+++ contracts/staking/src/bin/schemas.rs
@@ -1,13 +1,11 @@
 use cosmwasm_schema::write_api;
 
-use::staking::msg::{ExecuteMsg,QueryMsg,InstantiateMsg}
+use::staking::msg::{ExecuteMsg,QueryMsg,InstantiateMsg};
 
-
-
-fn main() {
-    write_api! {                                   
+fn main(){
+    write_api!{
         instantiate: InstantiateMsg,
         execute: ExecuteMsg,
         query: QueryMsg,
-     }
+    }
 }
--- target/debug/deps/schemas-9f5ece61f331ded3.d
+++ target/debug/deps/schemas-9f5ece61f331ded3.d
@@ -3,3 +3,6 @@
 /Users/saranevespereira/Documents/WBA_bootcamp/capstone/cw-angel-staking-0.2.4/target/debug/deps/schemas-9f5ece61f331ded3.d: contracts/staking/src/bin/schemas.rs
 
 contracts/staking/src/bin/schemas.rs:
+
+# env-dep:CARGO_PKG_NAME=staking
+# env-dep:CARGO_PKG_VERSION=0.1.0
